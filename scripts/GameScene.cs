da5797371202c622f6f525221fe2b73d13e6aa94
Luka687 <lb20201020@student.fon.bg.ac.rs>
2024-11-12 17:33:08 +0100

    Added proto-tileset, island generation

--- scripts/GameScene.cs
+++ scripts/GameScene.cs
@@ -3,15 +3,18 @@ using System;
 
 public partial class GameScene : Node2D
 {
+	
+	private TileMapLayer tileMapLayer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var httpRequest = new HttpRequest();
 		AddChild(httpRequest);
 		httpRequest.RequestCompleted += HttpRequestCompleted;
-		
 		httpRequest.Request("https://jobfair.nordeus.com/jf24-fullstack-challenge/test");
 		
+		tileMapLayer = GetNode<TileMapLayer>("TileMapLayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,8 +26,8 @@ public partial class GameScene : Node2D
 	private void HttpRequestCompleted(long result, long responseCode, string[] headers, byte[] body){
 		if (responseCode == 200){
 			//GD.Print("Request successful!");
-			GD.Print("Response Body: " + System.Text.Encoding.UTF8.GetString(body));
-			createMap(System.Text.Encoding.UTF8.GetString(body), 30, 30);
+			//GD.Print("Response Body: " + System.Text.Encoding.UTF8.GetString(body));
+			generateTileMap(createMap(System.Text.Encoding.UTF8.GetString(body), 30, 30));
 		}
 		
 		else{
@@ -47,5 +50,44 @@ public partial class GameScene : Node2D
 			index.Item1+= 1;
 		}
 		return map;
+	}
+	
+	private void generateTileMap(int [,] mapArray){
+		int mapWidth = mapArray.GetLength(0);
+		int mapHeight = mapArray.GetLength(1);
+		
+		for (int x = 0; x < mapWidth; x++)
+		{
+			for (int y = 0; y < mapHeight; y++)
+			{
+				tileMapLayer.SetCell(new Vector2I(x, y), 2, getTileType(mapArray[x,y]));
+			}
+		}
+	}
+	
+	
+	private Vector2I getTileType(int height){
+		switch (height){
+			case 0:
+				return new Vector2I(0,0);
+				break;
+			case <200:
+				return new Vector2I(1,0);
+				break;
+			case <400:
+				return new Vector2I(2,0);
+				break;
+			case <600:
+				return new Vector2I(3,0);
+				break;
+			case <800:
+				return new Vector2I(4,0);
+				break;
+			case <= 1000:
+				return new Vector2I(5,0);
+				break;
+			default:
+				return new Vector2I(1,0);
+		}
 	}
 }
